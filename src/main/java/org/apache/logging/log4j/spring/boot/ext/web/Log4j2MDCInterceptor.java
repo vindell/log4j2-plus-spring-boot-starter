d6b594b3a53f18e99e05d369dec31af17d63f13a
luffy <luffy@vanillafinance.xyz>
2024-11-03 23:44:19 +0800

    3.3.x.20241003.RELEASE

--- src/main/java/org/apache/logging/log4j/spring/boot/ext/web/Log4j2MDCInterceptor.java
+++ src/main/java/org/apache/logging/log4j/spring/boot/ext/web/Log4j2MDCInterceptor.java
@@ -3,8 +3,8 @@ package org.apache.logging.log4j.spring.boot.ext.web;
 import java.util.Enumeration;
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.ThreadContext;
 import org.apache.logging.log4j.spring.boot.utils.RemoteAddrUtils;
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 public class Log4j2MDCInterceptor implements HandlerInterceptor {
 
+	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 
 			throws Exception {
@@ -58,10 +59,12 @@ public class Log4j2MDCInterceptor implements HandlerInterceptor {
 		return true;
 	}
 
+	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 	}
 
+	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
 			Exception exception) throws Exception {
 		
--- src/main/java/org/apache/logging/log4j/spring/boot/utils/RemoteAddrUtils.java
+++ src/main/java/org/apache/logging/log4j/spring/boot/utils/RemoteAddrUtils.java
@@ -1,6 +1,6 @@
 package org.apache.logging.log4j.spring.boot.utils;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.util.StringUtils;
 
